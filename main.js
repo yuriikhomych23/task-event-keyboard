e6e21f7f3920d907c533681ec61a7e65545226b4
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-12 17:35:06 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-const firstColumn = document.querySelector('.first__column').children;
-
-for (let i = 0, child; child = firstColumn[i]; i++) {
-     window.addEventListener('keydown', function(event) {
-        if(event.keyCode == 192) {
-            area.innerText = '`';
-        } else if (event.keyCode == 49) {
-            area.innerText = '1';
-        } else if(event.keyCode == 50) {
-            area.innerText = '2'
-        } else if(event.keyCode == 51) {
-            area.innerText = '3'
-    }
+window.addEventListener('keydown', function(event) {
+    area.value += event.key;
 })
-}
 
 
 
@@ -21,15 +9,37 @@ for (let i = 0, child; child = firstColumn[i]; i++) {
 
 
 
-// const secondColumn = document.querySelector('.second__column').children;
 
-// for (let i = 0, child; child = secondColumn[i]; i++) {
-//     window.addEventListener('keydown', function(event) {
-//         if(event.keyCode == 81) {
-//             area.innerText = 'q'
-//         }
-//         if(event.keyCode == 87) {
-//             area.innerText = 'w'
-//         }
-//     })
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(keyCode == 8) {
+
+    // } else if (keyCode == 9) {
+        
+    // } else if(keyCode == 13) {
+
+    // } else if (keyCode == 16) {
+
+    // } else if (keyCode == 17)
