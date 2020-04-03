ee33d509305f48e74eea120b08d2574d94783d2e
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-04-03 14:09:41 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,45 +1,28 @@
-window.addEventListener('keydown', function(event) {
-    area.value += event.key;
+let getSel = x => document.querySelector(x);
+
+window.addEventListener('keydown', function(event){
+    if(event.keyCode == 9){
+        event.preventDefault();
+        getSel('.area').value += '\t';
+    }
+    if(event.keyCode != 16){
+        getSel(`.key${event.keyCode}`).style.backgroundColor = '#888';
+        getSel(`.key${event.keyCode}`).style.color = '#fff';
+    } else{
+        getSel(`.keyShift${event.location}`).style.backgroundColor = '#888';
+        getSel(`.keyShift${event.location}`).style.color = '#fff';
+    }
 })
 
+window.addEventListener('keyup', function(event){
+    if(event.keyCode != 16){
+        getSel(`.key${event.keyCode}`).style.backgroundColor = '';
+        getSel(`.key${event.keyCode}`).style.color = '';
+    } else{
+        getSel(`.keyShift${event.location}`).style.backgroundColor = '';
+        getSel(`.keyShift${event.location}`).style.color = '';
+    }
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(keyCode == 8) {
-
-    // } else if (keyCode == 9) {
-        
-    // } else if(keyCode == 13) {
-
-    // } else if (keyCode == 16) {
-
-    // } else if (keyCode == 17)
